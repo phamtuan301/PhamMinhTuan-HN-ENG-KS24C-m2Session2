eb3e523d80e38165fb95b90fbbb27054eabcdf0a
phamtuan301 <tuan13nt@gmail.com>
2024-11-21 00:03:50 +0700

    Add files via upload

---
+++ Session2-ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	int main(){
+		int firstNumber = 3;
+		int secondNumber = 5;
+		int tong = firstNumber + secondNumber;
+		int hieu = firstNumber + secondNumber;
+		float tich = firstNumber + secondNumber;
+		float thuong = firstNumber/secondNumber;
+		return 0;
+	}

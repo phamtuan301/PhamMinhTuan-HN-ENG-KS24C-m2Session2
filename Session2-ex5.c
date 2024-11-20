88d06ea68581fb40cd5c3899e85b324962c13615
phamtuan301 <tuan13nt@gmail.com>
2024-11-21 00:04:28 +0700

    Add files via upload

---
+++ Session2-ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+	int main(){
+		int chieudai = 6;
+		int chieurong = 4;
+		int chuvi = (chieudai + chieurong)*2;
+		int dientich = chieudai * chieurong;
+		printf("chu vi hcn la: %d\n", chuvi);
+		printf("dien tich hcn la: %d", dientich);
+		return 0;
+	}
+	

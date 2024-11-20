93de56fc30f1124a6f4bf8a68f1c652f000d7d87
phamtuan301 <tuan13nt@gmail.com>
2024-11-21 00:04:12 +0700

    Add files via upload

---
+++ Session2-ex4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+	int main(){
+		float x = 3;
+		float chuvi = x*4;
+		float dientich = x*x;
+		printf("chu vi hinh vuong la: %f\n", chuvi);
+		printf("dien tich hinh vuong la: %f", dientich);
+		return 0;
+	}

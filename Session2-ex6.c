fa1e3c4bf50584ba47a5ee44464bc81deed98a06
phamtuan301 <tuan13nt@gmail.com>
2024-11-21 00:04:46 +0700

    Add files via upload

---
+++ Session2-ex6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+	int main(){
+		float pi = 3.14;
+		int bankinh = 5;
+		float chuvi = bankinh * 2 * pi;
+		float dientich = bankinh * bankinh * pi;
+		printf("chu vi hinh tron la: %f\n", chuvi);
+		printf("dien tich hinh tron la: %f", dientich);
+		return 0;
+	}

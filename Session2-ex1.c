9e9684a84af777f4c4f14ea75438bab25b903f04
phamtuan301 <tuan13nt@gmail.com>
2024-11-21 08:53:59 +0700

    Add files via upload

---
+++ Session2-ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int songuyen=1;
+	// int la kieu du lieu de luu so nguyen
+	float sothuc=1.3456;
+	// float la kieu du lieu luu so thuc
+	char character = 'a';
+	// char dung de luu cac ki tu chu cai 
+	// kieu du lieu boolean the hien gia tri dung sai dung la 1 sai la 0
+	// kieu du lieu pointer thuong dung de luu tru dia chi cua bien khac
+	return 0;
+}
